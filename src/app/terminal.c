f047d98e885ad4f63081afc8f90c4ab85e8376a0
MuryloSaladino <murylosaladino@gmail.com>
2024-07-11 10:51:23 -0300

    feat: print assets

---
+++ src/app/terminal.c
@@ -0,0 +1,22 @@
+//
+// Created by muryl on 7/11/2024.
+//
+
+#include "terminal.h"
+
+char terminal_buffer[8192];
+
+void configureTerminal()
+{
+    setvbuf(stdout, terminal_buffer, _IOFBF, sizeof(terminal_buffer));
+
+#ifdef __WIN32
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+
+    GetConsoleMode(handle, &mode);
+    mode |= TERMINAL_PROCESSING;
+
+    SetConsoleMode(handle, mode);
+#endif
+}
